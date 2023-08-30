623ac5da66dc5798bffa8382006aa2b4430273a4
licazalilkz <licazalilkz@gmail.com>
2023-08-30 12:40:38 -0300

    dashboard

--- react-entrega-kenzie-hub-licazalilkz/src/pages/dashboard/dashboard.jsx
+++ react-entrega-kenzie-hub-licazalilkz/src/pages/dashboard/dashboard.jsx
@@ -14,7 +14,7 @@ export function Dashboard (){
     
     function logout(){
         localStorage.removeItem("@tokenKenzieHub");
-        window.location.reload(false);
+        navigate("/");
     }
 
     return(
